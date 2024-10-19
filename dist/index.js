981d3c8d8718a558827cc76fc56725a7f294859c
Grant Fortington <gwfortington@hotmail.com>
2024-10-19 21:07:52 +1030

    development

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.service = void 0;
 const class_1 = require("./class");
-const service = new class_1.Service('system-lookup-service', '_lookups', ['uuid'], ['lookup_type', 'meaning', 'description'], false);
-exports.service = service;
+exports.service = new class_1.Service('system-lookup-service', '_lookups', ['uuid'], ['lookup_type', 'meaning', 'description'], false);
